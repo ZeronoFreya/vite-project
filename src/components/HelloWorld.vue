2ca3c37e8c261150f3fd27da92c47c7c73d5e202
zeronofreya <zeronofreya@gmail.com>
2021-06-26 12:42:03 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.container {{msg}}
+</template>
+
+<script setup name="HelloWorld">
+import { defineProps, useContext } from "vue";
+
+const props = defineProps({
+    msg: {
+        type: String,
+        default: "",
+    },
+});
+
+const { expose, emit } = useContext();
+
+const demo = () => {
+    console.log("demo");
+};
+expose({
+    demo,
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+    width: 100%;
+    height: 100%;
+}
+</style>
